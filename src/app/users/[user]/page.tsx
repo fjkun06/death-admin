94dc0cc72f0117803d2ea54e87a413be98791493
= <=>
2024-04-15 15:16:26 +0100

    Basic scripts setup

--- src/app/users/[user]/page.tsx
+++ src/app/users/[user]/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Projects = () => {
+const Projects = ({params}:{params:{user:string}}) => {
 
-  return <main>user 2</main>
+  return <main>user 2 and  {params.user} </main>
 };
 
 export default Projects;
