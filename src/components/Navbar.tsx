07bae259d28bbcd4b9df0d545145d8f5e276a23d
fjkun06 <fjkun06@gmail.com>
2024-05-16 18:40:27 +0200

    Added custom user in localStorage useEffect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,18 @@
+"use client";
 import React from "react";
 import { NavLink } from "@/components";
+import { data } from "@/utils/localStorage";
 
 const Navbar = () => {
+  React.useEffect(() => {
+    /***Saving users to localstorage */
+    const users = localStorage.getItem("users");
+    if (users === null) {
+      localStorage.setItem("users", JSON.stringify(data));
+    }
+    console.log("hell-o: ", JSON.parse(users as string));
+  }, []);
+
   const links = [
     { label: "Impressum", route: "/impressum" },
     { label: "Datenschutzerklärung", route: "/datenschutz" },
---
+++ src/utils/localStorage.tsx
@@ -0,0 +1,54 @@
+
+export const data = [
+  {
+    user: "sofrzone1",
+    passwort: "Madhausse31",
+    token: "ARZ1A2B3",
+  },
+  {
+    user: "sofrzone2",
+    passwort: "StrongPass42",
+    token: "STA4D5E6",
+  },
+  {
+    user: "sofrzone3",
+    passwort: "UniquePwd53",
+    token: "GES7F8G9",
+  },
+  {
+    user: "sofrzone4",
+    passwort: "PassWord64",
+    token: "ARZA1B2C",
+  },
+  {
+    user: "sofrzone5",
+    passwort: "SafeKey75",
+    token: "STAD3E4F",
+  },
+  {
+    user: "sofrzone6",
+    passwort: "SecurePwd86",
+    token: "GES5G6H7",
+  },
+  {
+    user: "sofrzone7",
+    passwort: "MySecret97",
+    token: "ARZ8I9J0",
+  },
+  {
+    user: "sofrzone8",
+    passwort: "HiddenKey108",
+    token: "STAA2B3D",
+  },
+  {
+    user: "sofrzone9",
+    passwort: "LockKey219",
+    token: "GESB4C5E",
+  },
+  {
+    user: "sofrzone10",
+    passwort: "UniqueKey310",
+    token: "ARZ6F7G8",
+  },
+];
+
