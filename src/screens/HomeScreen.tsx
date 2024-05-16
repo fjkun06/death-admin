40f6d7b25e533e6946901c33a4e2c9f40cf24aa9
fjkun06 <fjkun06@gmail.com>
2024-05-16 18:18:22 +0200

    Styled Home Form

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,71 @@
+"use client";
 import { classNameGenerator } from "@/utils";
-import React from "react";
+import React, { useState } from "react";
 
 const HomeScreen = () => {
+  const [user, setUser] = useState("");
+  const [passwort, setPasswort] = useState("");
+  const [token, setToken] = useState("");
+
+  const handleUser = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser(e.target.value);
+    console.log(e.target.value);
+  };
+  const handlePasswort = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswort(e.target.value);
+    console.log(e.target.value);
+  };
+  const handleToken = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setToken(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const data = {user,passwort,token};
+  const resetAllFields = () => {
+    console.log(data);
+    setUser("");
+    setPasswort("");
+    setToken("");
+  };
   return (
     <main className={classNameGenerator("home")}>
       <h1>Digitales Todesbescheinigung App</h1>
+      <div className={classNameGenerator("home__login")}>
+        <h2>Bitte melden Sie sich an</h2>
+        <form action="">
+          <span>
+            <label htmlFor="user">Benutzername:</label>
+            <input
+              type="text"
+              name=""
+              id="user"
+              onChange={handleUser}
+              value={user}
+            />
+          </span>
+          <span>
+            <label htmlFor="password">Passwort:</label>
+            <input
+              type="password"
+              name=""
+              id="password"
+              onChange={handlePasswort}
+              value={passwort}
+            />
+          </span>
+          <span>
+            <label htmlFor="user">Token:</label>
+            <input
+              type="text"
+              name=""
+              id="token"
+              onChange={handleToken}
+              value={token}
+            />
+          </span>
+          <button type="button" onClick={resetAllFields}>Anmelden</button>
+        </form>
+      </div>
     </main>
   );
 };
